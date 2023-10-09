1ad9078fadf12c14b504813455a4d2ea748b8995
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2023-10-09 08:23:58 -0400

    Create review.py

---
+++ App/controllers/review.py
@@ -0,0 +1,15 @@
+from App.models import Review, Staff, Student
+from App.database import db
+
+def get_reviews_by_student(studentID):
+  return Review.query.filter_by(studentID=studentID).all()
+
+def get_reviews_by_staff(staffID):
+  return Review.query.filter_by(staffID=staffID).all()
+
+def delete_review(staffID, reviewID):
+  staff= Staff.query.get(staffID)
+  review = Review.query.get(reviewID)
+  if review and staff:
+    return staff.delete_review(review)
+  return False
