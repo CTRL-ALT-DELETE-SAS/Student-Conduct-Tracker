6bab65a106ccdbea26e8b3c64fd45c6d12ae18a0
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2023-10-09 06:09:11 -0400

    Updated auth.py

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -1,7 +1,7 @@
 from flask_login import login_user, login_manager, logout_user, LoginManager
 from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 
-from App.models import User
+from App.models import db, User, Staff,Student, Admin, 
 
 def jwt_authenticate(username, password):
   user = User.query.filter_by(username=username).first()
@@ -9,11 +9,70 @@ def jwt_authenticate(username, password):
     return create_access_token(identity=username)
   return None
 
-def login(username, password):
-    user = User.query.filter_by(username=username).first()
-    if user and user.check_password(password):
-        return user
+def login_staff(password, staffID):
+    staff = Staff.query.filter_by(staffID= staffID).first()
+    if staff and staff.check_password(password):
+        login_user(staff)
+        return staff
     return None
+
+def login_student(password, studentID):
+    student = Student.query.filter_by(studentID= studentID).first()
+    if student and student.check_password(password):
+        login_user(student)
+        return student
+    return None
+  
+  def login_admin(password, staffID):
+    admin = Admin.query.filter_by(staffID= staffID).first()
+    if admin and admin.check_password(password):
+        login_user(admin)
+        return admin
+    return None
+
+    def student_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated or not isinstance(current_user, Student):
+            return "Unauthorized", 401
+        return func(*args, **kwargs)
+    return wrapper
+
+    def staff_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated or not isinstance(current_user, Staff):
+            return "Unauthorized", 401
+        return func(*args, **kwargs)
+    return wrapper
+
+    def admin_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_user.is_authenticated or not isinstance(current_user, Admin):
+            return "Unauthorized", 401
+        return func(*args, **kwargs)
+    return wrapper
+
+  def get_all_students_json():
+    students = Student.query.all()
+    if not student:
+      return []
+    students= [student.toDict() for student in students]
+    return students
+
+  def get_all_staff_json():
+    staffmembers = Staff.query.all()
+    if not staff:
+      return []
+    staffmembers= [staff.toDict() for staff in staffmembers]
+    return staffmembers
+
+  def get_all_students():
+    return Student.query.all()
+
+  def get_all_staff():
+    return Staff.query.all()
 
 def setup_flask_login(app):
     login_manager = LoginManager()
