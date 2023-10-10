b768d76501e2a928a54d25a8dea0923d05cb8de6
Aratakiyo <97922031+Aratakiyo@users.noreply.github.com>
2023-10-10 17:04:26 +0000

    get users

--- App/views/user.py
+++ App/views/user.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, jsonify, request, abort
+from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_login import current_user, login_required
 from App.controllers.user import (
     create_student,
     create_staff,
     create_user,
     get_staff,
+    get_all_users,
     get_student,
     is_staff,
     is_student,
     is_admin,
+    get_all_users_json,
     get_all_students,
     get_all_staff,
     update_student,
@@ -18,10 +20,37 @@ from App.database import db
 # Create a Blueprint for user views
 user_views = Blueprint("user_views", __name__, template_folder='../templates')
 
+@user_views.route('/users', methods=['GET'])
+def get_user_page():
+    users = get_all_users()
+    return render_template('users.html', users=users)
+
+@user_views.route('/api/users', methods=['GET'])
+def get_users_action():
+    users = get_all_users_json()
+    return jsonify(users)
+
+@user_views.route('/api/users', methods=['POST'])
+def create_user_endpoint():
+    data = request.json
+    create_user(data['firstname'], data['lastname'], data['password'])
+    return jsonify({'message': f"user {data['firstname']} created"})
+
+@user_views.route('/users', methods=['POST'])
+def create_user_action():
+    data = request.form
+    flash(f"User {data['firstname']} created!")
+    create_user(data['firstname'], data['lastname'], data['password'])
+    return redirect(url_for('user_views.get_user_page'))
+
+@user_views.route('/static/users', methods=['GET'])
+def static_user_page():
+  return send_from_directory('static', 'static-user.html')
+
 # Route to create a new student
 @user_views.route("/user/create_student", methods=["POST"])
 @login_required
-def create_student_route():
+def create_student_action():
     if current_user.is_admin:
         data = request.get_json()
         firstname = data.get("firstname")
@@ -45,7 +74,7 @@ def create_student_route():
 # Route to create a new staff member
 @user_views.route("/user/create_staff", methods=["POST"])
 @login_required
-def create_staff_route():
+def create_staff_action():
     if current_user.is_admin:
         data = request.get_json()
         firstname = data.get("firstname")
@@ -68,7 +97,7 @@ def create_staff_route():
 # Route to create a new admin
 @user_views.route("/user/create_admin", methods=["POST"])
 @login_required
-def create_admin_route():
+def create_admin_action():
     if current_user.is_admin:
         data = request.get_json()
         firstname = data.get("firstname")
@@ -96,7 +125,7 @@ def get_staff_route(id):
 # Route to get a student by ID
 @user_views.route("/user/student/<string:id>", methods=["GET"])
 @login_required
-def get_student_route(id):
+def get_student_action(id):
     student = get_student(id)
     if student:
         return jsonify(student.to_json()), 200
@@ -106,25 +135,25 @@ def get_student_route(id):
 # Route to check if a user is a staff member
 @user_views.route("/user/is_staff/<string:id>", methods=["GET"])
 @login_required
-def is_staff_route(id):
+def is_staff_action(id):
     return jsonify({"is_staff": is_staff(id)}), 200
 
 # Route to check if a user is a student
 @user_views.route("/user/is_student/<string:id>", methods=["GET"])
 @login_required
-def is_student_route(id):
+def is_student_action(id):
     return jsonify({"is_student": is_student(id)}), 200
 
 # Route to check if a user is an admin
 @user_views.route("/user/is_admin/<string:id>", methods=["GET"])
 @login_required
-def is_admin_route(id):
+def is_admin_action(id):
     return jsonify({"is_admin": is_admin(id)}), 200
 
 # Route to get all students
 @user_views.route("/user/students", methods=["GET"])
 @login_required
-def get_all_students_route():
+def get_all_students_action():
     students = get_all_students()
     if students:
         return jsonify([student.to_json() for student in students]), 200
@@ -134,7 +163,7 @@ def get_all_students_route():
 # Route to get all staff members
 @user_views.route("/user/staff", methods=["GET"])
 @login_required
-def get_all_staff_route():
+def get_all_staff_action():
     staff = get_all_staff()
     if staff:
         return jsonify([s.to_json() for s in staff]), 200
@@ -144,7 +173,7 @@ def get_all_staff_route():
 # Route to update a student's information
 @user_views.route("/user/update_student/<string:student_id>", methods=["PUT"])
 @login_required
-def update_student_route(student_id):
+def update_student_action(student_id):
     if current_user.is_admin or (
         current_user.is_staff and current_user.staffID == student_id
     ):
