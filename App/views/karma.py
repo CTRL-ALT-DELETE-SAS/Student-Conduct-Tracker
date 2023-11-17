452b84f5a2c465270668761b9f23f06659499681
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2023-11-17 00:26:07 -0400

    Update karma.py REST conventions

--- App/views/karma.py
+++ App/views/karma.py
@@ -10,7 +10,7 @@ from App.controllers.karma import (
 karma_views = Blueprint("karma_views", __name__, template_folder='../templates')
 
 # Route to update Karma rankings for all students
-@karma_views.route("/karma/rankings", methods=["POST"])
+@karma_views.route("/karma/ranking", methods=["POST"])
 def update_karma_rankings_route():
     update_student_karma_rankings()
     return "Karma rankings updated", 200
