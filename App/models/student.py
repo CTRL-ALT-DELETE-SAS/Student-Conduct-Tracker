f142ed8e8dc20ce5cbaeb4b6dd70cb3b55c7797d
SarahG <97921763+SG765@users.noreply.github.com>
2023-11-24 00:39:39 +0000

    Implement model fixes for admin and student

--- App/models/student.py
+++ App/models/student.py
@@ -1,23 +1,23 @@
+from datetime import datetime
 from App.database import db
-from .user import User
 
-
-class Student(User):
+class Student(db.Model):
 	__tablename__ = 'student'
 	ID = db.Column(db.String(10), primary_key=True)
-	contact = db.Column(db.String(30), nullable=False)
+	firstname = db.Column(db.String(120), nullable=False)
+	lastname = db.Column(db.String(120), nullable=False)
 	studentType = db.Column(db.String(30))  #full-time, part-time or evening
-	yearOfStudy = db.Column(db.Integer, nullable=False)
+	yearOfEnrollment = db.Column(db.Integer)
 	reviews = db.relationship('Review', backref='student', lazy='joined')
 	karmaID = db.Column(db.Integer, db.ForeignKey('karma.karmaID'))
 
   #When student is newly created there would be no reviews or karma yet
-	def __init__(self, studentID, firstname, lastname, password, contact, studentType, yearofStudy):
-		super().__init__(firstname, lastname, password)
+	def __init__(self, studentID, firstname, lastname, studentType, yearofEnrollment):
 		self.ID = studentID
-		self.contact = contact
+		self.firstname = firstname
+		self.lastname = lastname
 		self.studentType = studentType
-		self.yearOfStudy = yearofStudy
+		self.yearOfEnrollment = yearofEnrollment
 		self.reviews = []
 	
 	def get_id(self):
@@ -30,11 +30,11 @@ class Student(User):
         "studentID": self.ID,
         "firstname": self.firstname,
         "lastname": self.lastname,
-        "contact": self.contact,
         "studentType": self.studentType,
-        "yearOfStudy": self.yearOfStudy,
+		"yearOfEnrollment": self.yearOfEnrollment,
+        "yearOfStudy": (datetime.now().year) - self.yearOfEnrollment, #Dynamically calculate year based on enrollment date
         "reviews": [review.to_json() for review in self.reviews],
-				"karmaScore": karma.score if karma else None,
+		"karmaScore": karma.score if karma else None,
         "karmaRank": karma.rank if karma else None,
     }
 
