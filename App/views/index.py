83c7fb14263bdb594edf432aa00c27e77c7e7a2b
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2023-11-30 18:57:23 -0400

    Removed create_student from the imports to try to resolve conflicts

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,7 @@
 import random
 from flask import Blueprint, render_template, jsonify
 from App.models import db
-from App.controllers import create_user, create_staff, create_student
+from App.controllers import create_user, create_staff
 import randomname
 
 from App.models.admin import Admin
