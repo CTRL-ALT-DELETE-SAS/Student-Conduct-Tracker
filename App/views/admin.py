a1fa1afb8dd65c6b8048a0ad6cd701ba0481bd1f
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2023-12-01 19:22:25 -0400

    Fixed add students and did update students

--- App/views/admin.py
+++ App/views/admin.py
@@ -54,10 +54,10 @@ def add_students():
         file_content = fp.read()
   
         for row in file_content:
-          if (row['studentType'] != "Full-Time" or row['studentType'] != "Part-Time" or row['studentType'] != "Evening" or or row['studentType'] != "Graduated" or row['studentType'] != "On-Leave"):
-            return jsonify({'error':f"{row['studentType']} was not a valis option"}), 400
+          if (row['studentType'] != "Full-Time" or row['studentType'] != "Part-Time" or row['studentType'] != "Evening" or row['studentType'] != "Graduated" or row['studentType'] != "On-Leave"):
+            return jsonify({'error':f"{row['studentType']} was not a valid option"}), 400
           else:
-              student=add_student_information(admin=user,id=row['id'],firstname=row['firstname'],lastname=row['lastname'],studentType=row['studentType'],yearofEnrollmentrow=row['yearofEnrollment'])
+              student=add_student_information(admin=jwt_current_user,id=row['id'],firstname=row['firstname'],lastname=row['lastname'],studentType=row['studentType'],yearofEnrollmentrow=row['yearofEnrollment'])
               if student:
                   return jsonify(student)
               else:
@@ -66,6 +66,41 @@ def add_students():
   
   return jsonify({'message': file_content}), 200
 
+#Route to batch update students via file upload
 @admin_views.route('/students', methods=['PUT'])
 def update_students():
+    if not jwt_current_user or not isinstance(jwt_current_user, Admin):
+      return 'Unauthorized', 401
+
+     # Check if the POST request has a file
+  if 'file' not in request.files:
+      return jsonify({'error': 'No file selected'}), 400
+  
+  file = request.files['file']
+
+  if file.filename == '':
+        return jsonify({'error': 'No file selected'}), 400
+  
+  if file and allowed_file(file.filename):
+    filename = secure_filename(file.filename)
+    # Save the file to the upload folder
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    # Read the content of the file
+    with open(file_path, 'r') as fp:
+        file_content = fp.read()
+  
+        for row in file_content:
+          if (row['field_to-update'] == "studentType"):
+              if (row['new_value'] != "Full-Time" or row['new_value'] != "Part-Time" or row['new_value'] != "Evening" or row['new_value'] != "Graduated" or row['new_value'] != "On-Leave"):
+            return jsonify({'error':f"{row['new_value']} was not a valid option"}), 400
+          else:
+              student=update_student(admin=jwt_current_user,id=row['id'],field_to_update=row['field_to-update'],new_value=row['new_valluw'])
+              if student:
+                  return jsonify(student)
+              else:
+                  return jsonify({'error': f"There is no student with ID {row['ID']} "}), 400
+    return jasonify({'message': f"database Updated"})
+    
   return jsonify({'message': f"Students updated"})
